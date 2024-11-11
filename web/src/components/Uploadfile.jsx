7e1e8a0c8b4b7262ad21aa43d0c910fcf2a76f28
6513177-Ananda <ananda.kon@student.mahidol.edu>
2024-11-12 01:28:36 +0700

    Finish Uploadfile

--- web/src/components/Uploadfile.jsx
+++ web/src/components/Uploadfile.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Uploadfile = () => {
+  const fileToDataUri = (file) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      resolve(event.target.result)
+    };
+    reader.readAsDataURL(file);
+    })
+
+    const [dataUri, setDataUri] = useState('')
+
+    const onChange = (file) => {
+      
+      if(!file) {
+        setDataUri('');
+        return;
+      }
+  
+      fileToDataUri(file)
+        .then(dataUri => {
+          setDataUri(dataUri)
+        })
+    }
+      
+
+
   return (
-    <div class="flex items-center justify-center w-full">
+    <div class="flex items-center justify-center w-full flex-col">
         <label for="dropzone-file" class="flex flex-col items-center justify-center w-2/5 h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
             <div class="flex flex-col items-center justify-center pt-5 pb-6">
                 <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
@@ -11,8 +36,9 @@ const Uploadfile = () => {
                 <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
                 <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
             </div>
-            <input id="dropzone-file" type="file" class="hidden" />
+            <input id="dropzone-file" type="file" class="hidden" onChange={(event) => onChange(event.target.files[0] || null)}/>
         </label>
+        <img width="200" height="200" src={dataUri} alt="avatar" className='p-4'/>
     </div> 
   )
 }
