d0914e010ba3611e87dd785509a0da658381a60c
rrrrrrrjk <ryw.jakkraphat@gmail.com>
2024-11-13 02:00:08 +0700

    Make Upload files plane

--- web/src/pages/Demo.jsx
+++ web/src/pages/Demo.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import Uploadfile from '../components/Uploadfile'
+import Model from '../sections/Model'
 
 export const Demo = () => {
   return (
-    <section className='min-h-screen w-full flex justify-start items-center px-[50px]'>
-      <div className='w-[262px] h-[594px] bg-white p-[8px] rounded-[41px] bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-60'>
-        <Uploadfile/>
-      </div>
-      
+    <section>
+      <Model/>
     </section>
   )
 }
---
+++ web/src/sections/Model.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Uploadfile from '../components/Uploadfile'
+
+const Model = () => {
+  return (
+    <section className='h-auto w-full flex justify-start items-center px-[50px]'>
+        <div className='w-[262px] h-[594px] bg-inherit shadow-[6px_6px_30px] p-[8px] rounded-[41px] bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-60'>
+        <Uploadfile/>
+      </div>
+    </section>
+  )
+}
+
+export default Model
