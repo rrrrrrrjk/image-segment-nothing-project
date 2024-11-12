d132d6846795a11574d87725a587d09dbe9e5275
rrrrrrrjk <ryw.jakkraphat@gmail.com>
2024-11-12 23:47:45 +0700

    Train model on COCO 111K datasets

--- python/main.py
+++ python/main.py
@@ -1,8 +1,5 @@
-from ultralytics import YOLO, checks, hub
-if __name__ == '__main__':
-    checks()
+from ultralytics import YOLO
 
-    hub.login('b4b855396ff84d51061ad93b3aa1a19c6a877e5ee9')
+model = YOLO("lastest-flower.pt") 
 
-    model = YOLO('https://hub.ultralytics.com/models/hKAGsbSiX8PrMz0vCO8c')
-    results = model.train()
+results = model.train(data="coco-seg.yaml", epochs=100, imgsz=640, resume=True)
