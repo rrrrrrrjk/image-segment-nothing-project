ab30164eef66dd4d5db26cdab5c863bc61dda717
6513177-Ananda <ananda.kon@student.mahidol.edu>
2024-11-15 00:08:22 +0700

    Just extract file

--- web/src/components/ImageSegment.jsx
+++ web/src/components/ImageSegment.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "../sass/components/ImageSegment.scss"
+import * as tf from '@tensorflow/tfjs';
+import * as tfn from '@tensorflow/tfjs-node';
 
-const Imagesegment = ({sentImage}) => {
+
+async function loadModels() {
+  const handler = tfn.io.fileSystem('../Tensor/model.json');
+  const model = await tf.loadLayersModel(handler);
+  return model
+}
+
+const Imagesegment = ({ sentImage }) => {
+  useEffect(() => {
+    const model = loadModels();
+    model.then((model) => {
+      model.estimateFaces(sentImage).then((predictions) => {
+        console.log(predictions);
+      });
+    });
+  }, [sentImage]);
+
+
+
   return (
     <>
-        <img src={sentImage} alt="" className='Image-Segment'/>
+      <img src={sentImage} alt="" className='Image-Segment' />
     </>
   )
 }
